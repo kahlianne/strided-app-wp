3b98b1dc112e37f0b5e85e80f93c35158198a702
Raquel Smith <raquel@foodbloggerpro.com>
2018-04-07 16:03:04 -0700

    Properly show arena address

--- admin/partials/arena-admin-display.php
+++ admin/partials/arena-admin-display.php
@@ -22,6 +22,6 @@ $arena_info = get_post_meta( get_the_ID() );
 	<input type="hidden" name="arena_information_meta_box_nonce" value="<?php esc_attr_e( wp_create_nonce('arena_information_meta_box_nonce') ); ?>" />
 
 	<label for="arena_address">Address</label>
-	<textarea id="arena_address" name="arena_address" value="<?php esc_attr_e( $arena_info[ '_arena_address' ][0] ); ?>" /></textarea><br />
+	<textarea id="arena_address" name="arena_address"><?php esc_attr_e( $arena_info[ '_arena_address' ][0] ); ?></textarea><br />
 
 </div>
