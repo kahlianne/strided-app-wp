da5c8fd565a4c440b0c65926863a38e9e1adac82
Raquel Smith <raquel@foodbloggerpro.com>
2018-04-17 20:57:45 -0700

    Filter cpt display to include custom meta

--- public/class-strided-app-public.php
+++ public/class-strided-app-public.php
@@ -102,8 +102,70 @@ class Strided_App_Public {
 	public function filter_the_content_strided_post_type( $content ) {
 		global $post;
 		$post_type = get_post_type( $post );
+		$post_meta = get_post_meta( $post->ID );
 		if ( 'horse' == $post_type ) {
-			return $content . 'this is a horse';
+			$registered_name = $post_meta['_horse_registered_name'][0];
+			$year_born = $post_meta['_horse_year_born'][0];
+			$gender = $post_meta['_horse_gender'][0];
+			$markup = '<div class="horse-information">';
+			if ( isset( $registered_name ) ) {
+				$markup .= '<div class="horse-registered-name"><span class="label">Registered Name:</span> ' . esc_html( $registered_name ) . '</div>';
+			}
+			if ( isset( $year_born ) ) {
+				$markup .= '<div class="horse-year-born"><span class="label">Year Born:</span> ' . esc_html( $year_born ) . '</div>';
+			}
+			if ( isset( $gender ) ) {
+				$markup .= '<div class="horse-gender"><span class="label">Horse Gender:</span> ' . esc_html( $gender ) . '</div>';
+			}
+			$markup .= '<div class="horse-description">' . $content . '</div></div>';
+			return $markup;
+		}
+		if ( 'arena' == $post_type ) {
+			$address = $post_meta['_arena_address'][0];
+			$markup = '<div class="arena-information">';
+			if ( isset( $address ) ) {
+				$markup .= '<div class="arena-address"><span class="label">Address:</span> ' . esc_html( $address ) . '</div>';
+			}
+			$markup .= '<div class="arena-description">' . $content . '</div></div>';
+			return $markup;
+		}
+		if ( 'run' == $post_type ) {
+			$arena = get_the_title( $post_meta['_run_arena'][0] );
+			$arena_url = get_permalink( $post_meta['_run_arena'][0] );
+			$horse = get_the_title( $post_meta['_run_horse'][0] );
+			$horse_url = get_permalink( $post_meta['_run_horse'][0] );
+			$time = $post_meta['_run_time'][0];
+			$placing = $post_meta['_run_placing'][0];
+			$class = $post_meta['_run_class'][0];
+			$winnings = $post_meta['_run_winnings'][0];
+			if ( isset( $post_meta[ '_run_video' ][0] ) && null != $post_meta[ '_run_video' ][0] ) {
+				$video_number = preg_match('/\d{9}/', $post_meta[ '_run_video' ][0], $matches);
+			}
+			$markup = '<div class="horse-information">';
+			if ( isset( $arena ) ) {
+				$markup .= '<div class="run-arena"><span class="label">Arena:</span> <a href="' . esc_url( $arena_url ) . '">' . esc_html( $arena ) . '</a></div>';
+			}
+			if ( isset( $horse ) ) {
+				$markup .= '<div class="run-horse"><span class="label">Horse:</span> <a href="' . esc_url( $horse_url ) . '">' . esc_html( $horse ) . '</a></div>';
+			}
+			if ( isset( $time ) ) {
+				$markup .= '<div class="run-time"><span class="label">Time:</span> ' . esc_html( $time ) . ' seconds</div>';
+			}
+			if ( isset( $placing ) ) {
+				$markup .= '<div class="run-placing"><span class="label">Placing:</span> ' . esc_html( $placing ) . '</div>';
+			}
+			if ( isset( $class ) ) {
+				$markup .= '<div class="run-class"><span class="label">Class:</span> ' . esc_html( $class ) . '</div>';
+			}
+			if ( isset( $winnings ) ) {
+				$markup .= '<div class="run-winnings"><span class="label">Winnings:</span> $' . esc_html( $winnings ) . '</div>';
+			}
+			$markup .= '<div class="run-description">' . $content . '</div>';
+			if ( isset( $matches[0] ) ) {
+				$markup .= '<div class="run-video"><iframe src="https://player.vimeo.com/video/' . $matches[0] . '" width="254" height="auto" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>';
+			}
+			$markup .= '</div>';
+			return $markup;
 		}
 		return $content;
 	}
