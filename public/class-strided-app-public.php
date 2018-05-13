0fdf88140022242c03c870680fabbd94ffecdae3
Raquel Smith <raquel@foodbloggerpro.com>
2018-05-13 16:39:35 -0700

    Allow FE-editing for most fields for all cpts

--- public/class-strided-app-public.php
+++ public/class-strided-app-public.php
@@ -77,14 +77,18 @@ class Strided_App_Public {
 	}
 
 	public function filter_add_horse_field_options( $options, $settings ){
-		if( 'horse' == $settings['key'] || 'arena' == $settings['key'] ){
+		if( 'horse' == $settings['key'] || 'arena' == $settings['key'] || 'edit_run_arena' == $settings['key'] || 'edit_run_horse' == $settings['key'] ){
+			$post_type = 'arena';
+			if ( $settings['key'] == 'horse' || 'edit_run_horse' == $settings['key'] ) {
+				$post_type = 'horse';
+			}
 			$args = array(
-				'author' => get_current_user_id(),
-				'post_type' => $settings['key'],
-				'orderby' => 'menu_order',
-				'order' => 'ASC',
+				'author'         => get_current_user_id(),
+				'post_type'      => $post_type,
+				'orderby'        => 'menu_order',
+				'order'          => 'ASC',
 				'posts_per_page' => -1,
-				'post_status' => 'publish'
+				'post_status'    => 'publish'
 			);
 			$the_query = new WP_Query( $args ); 
 			if ( $the_query->have_posts() ){
@@ -98,96 +102,153 @@ class Strided_App_Public {
 		}
 		return $options;
 	}
+	public function create_markup_horse_display( $content, $post, $post_meta ) {
+		$markup = '';
+		$horse_name = $post->post_title;
+		$registered_name = $post_meta['_horse_registered_name'][0];
+		$year_born = $post_meta['_horse_year_born'][0];
+		$gender = $post_meta['_horse_gender'][0];
+		$image_url = get_the_post_thumbnail_url( $post->ID );
+		$edit_url = add_query_arg( array( 
+				'post'            => $post->ID,
+				'horse-name'      => $horse_name,
+				'registered-name' => $registered_name,
+				'year-born'       => $year_born,	
+				'gender'          => $gender,
+				'description'     => wp_strip_all_tags( $content ),
+				'image-url'       => $image_url,
+				'post-type'       => 'horse'
+			), 
+			home_url( 'view-all-horses/edit-horse/' ) );
+		if ( current_user_can( 'edit_post', $post->ID ) ) {
+			$markup .= '<div class="post-actions"><a href="' 
+				. esc_url( $edit_url ) 
+				. '"><button>Edit</button></a><a onclick="return confirm(\'Are you sure you wish to delete this?\')" href="' 
+				. get_delete_post_link( $post->ID ) 
+				. '"><button>Delete</button></a></div>';
+				//TODO: redirect after deleting post
+		}
+		$markup .= '<div class="horse-information">';
+		if ( null != $registered_name ) {
+			$markup .= '<div class="horse-registered-name"><span class="label">Registered Name:</span> ' . esc_html( $registered_name ) . '</div>';
+		}
+		if ( null != $year_born ) {
+			$markup .= '<div class="horse-year-born"><span class="label">Year Born:</span> ' . esc_html( $year_born ) . '</div>';
+		}
+		if ( null != $gender ) {
+			$markup .= '<div class="horse-gender"><span class="label">Horse Gender:</span> ' . esc_html( $gender ) . '</div>';
+		}
+		$markup .= '<div class="horse-description">' . $content . '</div></div>';
+		return $markup;
+	}
+
+	public function create_markup_arena_display( $content, $post, $post_meta ) {
+		$markup = '';
+		$arena_name = $post->post_title;
+		$address = $post_meta['_arena_address'][0];
+		$edit_url = add_query_arg( array( 
+				'post'          => $post->ID,
+				'arena-name'    => $arena_name,
+				'arena-address' => $address,
+				'description'   => wp_strip_all_tags( $content ),
+				'post-type'     => 'arena'
+			), 
+			home_url( 'view-all-horses/edit-arena/' ) );
+		if ( current_user_can( 'edit_post', $post->ID ) ) {
+			$markup .= '<div class="post-actions"><a href="' 
+				. esc_url( $edit_url ) 
+				. '"><button>Edit</button></a><a onclick="return confirm(\'Are you sure you wish to delete this?\')" href="' 
+				. get_delete_post_link( $post->ID ) 
+				. '"><button>Delete</button></a></div>';
+				//TODO: redirect after deleting post
+		}
+		$markup .= '<div class="arena-information">';
+		if ( null != $address ) {
+			$markup .= '<div class="arena-address"><span class="label">Address:</span> ' . esc_html( $address ) . '</div>';
+		}
+		$markup .= '<div class="arena-description">' . $content . '</div></div>';
+		return $markup;
+	}
+
+	public function create_markup_run_display( $content, $post, $post_meta ) {
+		$markup = '';
+		$run_name = $post->post_title;
+		$date = $post_meta['_run_date'][0];
+		$arena = $post_meta['_run_arena'][0];
+		$horse = $post_meta['_run_horse'][0];
+		$time = $post_meta['_run_time'][0];
+		$placing = $post_meta['_run_placing'][0];
+		$class = $post_meta['_run_class'][0];
+		$winnings = $post_meta['_run_winnings'][0];
+		$video_url = $post_meta[ '_run_video' ][0];
+		$edit_url = add_query_arg( array( 
+				'post'        => $post->ID,
+				'arena-name'  => $arena,
+				'horse-name'  => $horse,
+				'time'        => $time,	
+				'placing'     => $placing,
+				'class'       => $class,
+				'winnings'    => $winnings,
+				'description' => wp_strip_all_tags( $content ),
+				'video-url'   => $video_url,
+				'post-type'   => 'run'
+			), 
+			home_url( 'view-all-runs/edit-run' ) );
+		if ( current_user_can( 'edit_post', $post->ID ) ) {
+			$markup .= '<div class="post-actions"><a href="' 
+				. esc_url( $edit_url ) 
+				. '"><button>Edit</button></a><a onclick="return confirm(\'Are you sure you wish to delete this?\')" href="' 
+				. get_delete_post_link( $post->ID ) 
+				. '"><button>Delete</button></a></div>';
+				//TODO: redirect after deleting post
+		}
+		$markup .= '<div class="run-information">';
+		if ( null != $date ) {
+			$markup .= '<div class="run-date"><span class="label">Date: </span> ' . esc_html( $date ) . '</div>';
+		}
+		if ( null != $arena ) {
+			$arena_url = get_permalink( $arena );
+			$markup .= '<div class="run-arena"><span class="label">Arena:</span> <a href="' . esc_url( $arena_url ) . '">' . esc_html( get_the_title( $arena ) ) . '</a></div>';
+		}
+		if ( null != $horse ) {
+			$horse_url = get_permalink( $horse );
+			$markup .= '<div class="run-horse"><span class="label">Horse:</span> <a href="' . esc_url( $horse_url ) . '">' . esc_html( get_the_title( $horse ) ) . '</a></div>';
+		}
+		if ( null != $time ) {
+			$markup .= '<div class="run-time"><span class="label">Time:</span> ' . esc_html( $time ) . ' seconds</div>';
+		}
+		if ( null != $placing  ) {
+			$markup .= '<div class="run-placing"><span class="label">Placing:</span> ' . esc_html( $placing ) . '</div>';
+		}
+		if ( null != $class ) {
+			$markup .= '<div class="run-class"><span class="label">Class:</span> ' . esc_html( $class ) . '</div>';
+		}
+		if ( null != $winnings ) {
+			$markup .= '<div class="run-winnings"><span class="label">Winnings:</span> $' . esc_html( $winnings ) . '</div>';
+		}
+		$markup .= '<div class="run-description">' . $content . '</div>';
+		if ( isset( $post_meta[ '_run_video' ][0] ) && null != $post_meta[ '_run_video' ][0] ) {
+			$video_number = preg_match('/\d{9}/', $post_meta[ '_run_video' ][0], $matches);
+			if ( isset( $matches[0] ) ) {
+				$markup .= '<div class="run-video"><iframe src="https://player.vimeo.com/video/' . $matches[0] . '" width="254" height="auto" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>';
+			}
+		}
+		$markup .= '</div>';
+		return $markup;
+	}
 
 	public function filter_the_content_strided_post_type( $content ) {
 		global $post;
 		$post_type = get_post_type( $post );
 		$post_meta = get_post_meta( $post->ID );
-		$markup = '';
 		if ( 'horse' == $post_type ) {
-			$horse_name = $post->post_title;
-			$registered_name = $post_meta['_horse_registered_name'][0];
-			$year_born = $post_meta['_horse_year_born'][0];
-			$gender = $post_meta['_horse_gender'][0];
-			$image_url = get_the_post_thumbnail_url( $post->ID );
-			$edit_url = add_query_arg( array( 
-					'post' => $post->ID,
-					'horse-name' => $horse_name,
-					'registered-name' => $registered_name,
-					'year-born' => $year_born,	
-					'gender' => $gender,
-					'description' => wp_strip_all_tags( $content ),
-					'image-url' => $image_url,
-					'post-type' => 'horse'
-				), 
-				home_url( 'view-all-horses/edit-horse/' ) );
-			if ( current_user_can( 'edit_post', $post->ID ) ) {
-				$markup .= '<div class="post-actions"><a href="' 
-					. esc_url( $edit_url ) 
-					. '"><button>Edit</button></a><a onclick="return confirm(\'Are you sure you wish to delete this?\')" href="' 
-					. get_delete_post_link( $post->ID ) 
-					. '"><button>Delete</button></a></div>';
-					//TODO: redirect after deleting post
-			}
-			$markup .= '<div class="horse-information">';
-			if ( null != $registered_name ) {
-				$markup .= '<div class="horse-registered-name"><span class="label">Registered Name:</span> ' . esc_html( $registered_name ) . '</div>';
-			}
-			if ( null != $year_born ) {
-				$markup .= '<div class="horse-year-born"><span class="label">Year Born:</span> ' . esc_html( $year_born ) . '</div>';
-			}
-			if ( null != $gender ) {
-				$markup .= '<div class="horse-gender"><span class="label">Horse Gender:</span> ' . esc_html( $gender ) . '</div>';
-			}
-			$markup .= '<div class="horse-description">' . $content . '</div></div>';
-			return $markup;
+			return $this->create_markup_horse_display( $content, $post, $post_meta );
 		}
 		if ( 'arena' == $post_type ) {
-			$markup .= '<div class="arena-information">';
-			if ( null != $post_meta['_arena_address'][0] ) {
-				$address = $post_meta['_arena_address'][0];
-				$markup .= '<div class="arena-address"><span class="label">Address:</span> ' . esc_html( $address ) . '</div>';
-			}
-			$markup .= '<div class="arena-description">' . $content . '</div></div>';
-			return $markup;
+			return $this->create_markup_arena_display( $content, $post, $post_meta );
 		}
 		if ( 'run' == $post_type ) {
-			$markup .= '<div class="horse-information">';
-			if ( null != $post_meta['_run_arena'][0] ) {
-				$arena = get_the_title( $post_meta['_run_arena'][0] );
-				$arena_url = get_permalink( $post_meta['_run_arena'][0] );
-				$markup .= '<div class="run-arena"><span class="label">Arena:</span> <a href="' . esc_url( $arena_url ) . '">' . esc_html( $arena ) . '</a></div>';
-			}
-			if ( null != $post_meta['_run_horse'][0] ) {
-				$horse = get_the_title( $post_meta['_run_horse'][0] );
-				$horse_url = get_permalink( $post_meta['_run_horse'][0] );
-				$markup .= '<div class="run-horse"><span class="label">Horse:</span> <a href="' . esc_url( $horse_url ) . '">' . esc_html( $horse ) . '</a></div>';
-			}
-			if ( null != $post_meta['_run_time'][0] ) {
-				$time = $post_meta['_run_time'][0];
-				$markup .= '<div class="run-time"><span class="label">Time:</span> ' . esc_html( $time ) . ' seconds</div>';
-			}
-			if ( null != $post_meta['_run_placing'][0]  ) {
-				$placing = $post_meta['_run_placing'][0];
-				$markup .= '<div class="run-placing"><span class="label">Placing:</span> ' . esc_html( $placing ) . '</div>';
-			}
-			if ( null != $post_meta['_run_class'][0] ) {
-				$class = $post_meta['_run_class'][0];
-				$markup .= '<div class="run-class"><span class="label">Class:</span> ' . esc_html( $class ) . '</div>';
-			}
-			if ( null != $post_meta['_run_winnings'][0] ) {
-				$winnings = $post_meta['_run_winnings'][0];
-				$markup .= '<div class="run-winnings"><span class="label">Winnings:</span> $' . esc_html( $winnings ) . '</div>';
-			}
-			$markup .= '<div class="run-description">' . $content . '</div>';
-			if ( isset( $post_meta[ '_run_video' ][0] ) && null != $post_meta[ '_run_video' ][0] ) {
-				$video_number = preg_match('/\d{9}/', $post_meta[ '_run_video' ][0], $matches);
-				if ( isset( $matches[0] ) ) {
-					$markup .= '<div class="run-video"><iframe src="https://player.vimeo.com/video/' . $matches[0] . '" width="254" height="auto" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>';
-				}
-			}
-			$markup .= '</div>';
-			return $markup;
+			return $this->create_markup_run_display( $content, $post, $post_meta );
 		}
 		return $content;
 	}
@@ -217,8 +278,8 @@ class Strided_App_Public {
 			$attributes = array(
 				'radioControl' => 'run',
 				'rangeControl' => 100,
-				'metaKey' => '_run_horse',
-				'metaValue' => $post->ID
+				'metaKey'      => '_run_horse',
+				'metaValue'    => $post->ID
 			);
 			$content.= '<div class="runs-with-horse"><h3>Runs with ' . $post->post_title . ':</h3>';
 			$content .= Strided_App_Admin::strided_member_content_block_render( $attributes );
@@ -228,8 +289,8 @@ class Strided_App_Public {
 			$attributes = array(
 				'radioControl' => 'run',
 				'rangeControl' => 100,
-				'metaKey' => '_run_arena',
-				'metaValue' => $post->ID
+				'metaKey'      => '_run_arena',
+				'metaValue'    => $post->ID
 			);
 			$content.= '<div class="runs-at-arena"><h3>Runs at ' . $post->post_title . ':</h3>';
 			$content .= Strided_App_Admin::strided_member_content_block_render( $attributes );
@@ -238,42 +299,120 @@ class Strided_App_Public {
 		return $content;
 	}
 
-	public function action_edit_original_post_on_save( $form_data ) {
-		//if ( current_user_can( 'edit_post', $post_id_to_update ) ) {
-			$post_info = array();
-			$post_meta = array();
-			$form_id       = $form_data[ 'form_id' ];
-			$form_fields   =  $form_data[ 'fields' ];
-			foreach( $form_fields as $field ){
-				$field_id    = $field[ 'id' ];
-				$field_key   = $field[ 'key' ];
-				$field_value = $field[ 'value' ];
-				if ( 'edit_post_id' == $field_key ) {
-					$post_info['ID'] = $field_value;
-					$post_id_to_update = $field_value;
-				}
-				if ( 'edit_horse_name' == $field_key ) {
-					$post_info['post_title'] = $field_value;
-				}
-				if ( 'edit_horse_information' == $field_key ) {
-					$post_info['post_content'] = $field_value;
-				}
-				if ( 'edit_horse_registered_name' == $field_key ) {
-					$post_meta['_horse_registered_name'] = $field_value;
-				}
-				if ( 'edit_year_born' == $field_key ) {
-					$post_meta['_horse_year_born'] = $field_value;
-				}
-				if ( 'edit_horse_gender' == $field_key ) {
-					$post_meta['_horse_gender'] = $field_value;
-				}
+	public function action_edit_horse_post_on_save( $form_data ) {
+		$post_info = array();
+		$post_meta = array();
+		$form_id       = $form_data[ 'form_id' ];
+		$form_fields   =  $form_data[ 'fields' ];
+		foreach( $form_fields as $field ){
+			$field_id    = $field[ 'id' ];
+			$field_key   = $field[ 'key' ];
+			$field_value = $field[ 'value' ];
+			if ( 'edit_post_id' == $field_key ) {
+				$post_info['ID'] = $field_value;
+				$post_id_to_update = $field_value;
 			}
+			if ( 'edit_horse_name' == $field_key ) {
+				$post_info['post_title'] = $field_value;
+			}
+			if ( 'edit_horse_information' == $field_key ) {
+				$post_info['post_content'] = $field_value;
+			}
+			if ( 'edit_horse_registered_name' == $field_key ) {
+				$post_meta['_horse_registered_name'] = $field_value;
+			}
+			if ( 'edit_year_born' == $field_key ) {
+				$post_meta['_horse_year_born'] = $field_value;
+			}
+			if ( 'edit_horse_gender' == $field_key ) {
+				$post_meta['_horse_gender'] = $field_value;
+			}
+		}
+		if ( current_user_can( 'edit_post', $post_id_to_update ) ) {
 			wp_update_post( $post_info );
 			foreach ( $post_meta as $key => $value ) {
 				update_post_meta( $post_id_to_update, $key, $value );
 			}
-			$form_settings = $form_data[ 'settings' ];
-			$form_title    = $form_data[ 'settings' ][ 'title' ];
-		//}
+		}
+	}
+
+	public function action_edit_arena_post_on_save( $form_data ) {
+		$post_info = array();
+		$post_meta = array();
+		$form_id       = $form_data[ 'form_id' ];
+		$form_fields   =  $form_data[ 'fields' ];
+		foreach( $form_fields as $field ){
+			$field_id    = $field[ 'id' ];
+			$field_key   = $field[ 'key' ];
+			$field_value = $field[ 'value' ];
+			if ( 'edit_post_id' == $field_key ) {
+				$post_info['ID'] = $field_value;
+				$post_id_to_update = $field_value;
+			}
+			if ( 'edit_arena_name' == $field_key ) {
+				$post_info['post_title'] = $field_value;
+			}
+			if ( 'edit_arena_information' == $field_key ) {
+				$post_info['post_content'] = $field_value;
+			}
+			if ( 'edit_arena_address' == $field_key ) {
+				$post_meta['_arena_address'] = $field_value;
+			}
+		}
+		if ( current_user_can( 'edit_post', $post_id_to_update ) ) {
+			wp_update_post( $post_info );
+			foreach ( $post_meta as $key => $value ) {
+				update_post_meta( $post_id_to_update, $key, $value );
+			}
+		}
+	}
+
+	public function action_edit_run_post_on_save( $form_data ) {
+		$post_info = array();
+		$post_meta = array();
+		$form_id       = $form_data[ 'form_id' ];
+		$form_fields   =  $form_data[ 'fields' ];
+		foreach( $form_fields as $field ){
+			$field_id    = $field[ 'id' ];
+			$field_key   = $field[ 'key' ];
+			$field_value = $field[ 'value' ];
+			if ( 'edit_post_id' == $field_key ) {
+				$post_info['ID'] = $field_value;
+				$post_id_to_update = $field_value;
+			}
+			if ( 'edit_run_description' == $field_key ) {
+				$post_info['post_content'] = $field_value;
+			}
+			if ( 'edit_run_date' == $field_key ) {
+				$post_meta['_run_date'] = $field_value;
+			}
+			if ( 'edit_run_horse' == $field_key ) {
+				$post_meta['_run_horse'] = $field_value;
+			}
+			if ( 'edit_run_arena' == $field_key ) {
+				$post_meta['_run_arena'] = $field_value;
+			}
+			if ( 'edit_run_placing' == $field_key ) {
+				$post_meta['_run_placing'] = $field_value;
+			}
+			if ( 'edit_run_class' == $field_key ) {
+				$post_meta['_run_class'] = $field_value;
+			}
+			if ( 'edit_run_time' == $field_key ) {
+				$post_meta['_run_time'] = $field_value;
+			}
+			if ( 'edit_run_winnings' == $field_key ) {
+				$post_meta['_run_winnings'] = $field_value;
+			}
+			if ( 'edit_run_video' == $field_key ) {
+				$post_meta['_run_video'] = $field_value;
+			}
+		}
+		if ( current_user_can( 'edit_post', $post_id_to_update ) ) {
+			wp_update_post( $post_info );
+			foreach ( $post_meta as $key => $value ) {
+				update_post_meta( $post_id_to_update, $key, $value );
+			}
+		}
 	}
 }
--- public/js/strided-app-public.js
+++ public/js/strided-app-public.js
@@ -1,32 +1,26 @@
 (function( $ ) {
 	'use strict';
 
-	/**
-	 * All of the code for your public-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
+	$(document).ready( function() {
+
+		function getParameterByName(name, url) {
+			if (!url) url = window.location.href;
+			name = name.replace(/[\[\]]/g, "\\$&");
+			var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+			results = regex.exec(url);
+			if (!results) return null;
+			if (!results[2]) return '';
+			return decodeURIComponent(results[2].replace(/\+/g, " "));
+		}
+
+		var horse = getParameterByName('horse-name');
+		var arena = getParameterByName('arena-name');
+
+		setTimeout(function(){
+			$(".arena-select").val(arena);
+			$(".horse-select").val(horse);
+		}, 1000);
+
+	})
 
 })( jQuery );
